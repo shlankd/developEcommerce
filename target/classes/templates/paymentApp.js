5cf865334272093f10d98bed278b2d12bdbfdd17
shlankd <shlankd@gmail.com>
2024-12-31 13:20:17 +0200

    Add files via upload

---
+++ target/classes/templates/paymentApp.js
@@ -0,0 +1,24 @@
+//
+// $('#payButton').on('submit',function() {
+//     $.ajax({
+//         url: "http://localhost:8080/createOrderAndPayment",
+//         type: 'POST',
+//         contentType: 'application/json',
+//         headers: {
+//             "Authorization": "Bearer " + $('#authToken').val()
+//             // "Request Body": {
+//             //     "userId":$('#userId'),
+//             //     "addressId":$('#addressId'),
+//             //     "currency":$('#currency'),
+//             //     "description":$('#description')
+//             // }
+//         },
+//         body: {
+//             "userId":$('#userId'),
+//             "addressId":$('#addressId'),
+//             "currency":$('#currency'),
+//             "description":$('#description')
+//         },
+//         async: false
+//     })
+// });
